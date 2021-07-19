6b3afd9f175abe9df5ef3d0584a143c51edc06b4
BorysShvyryd <bormanpgg@gmail.com>
2021-07-19 19:47:07 +0300

    Zad3:Formularz logowania, AdminDao - metody autoryzacji

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -1,2 +1,24 @@
-package pl.coderslab.dao;public class AdminDao {
+package pl.coderslab.dao;
+
+import pl.coderslab.utils.DbUtil;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AdminDao {
+    private static final String SEARCH_FOR_EMAIL_AND_PASS_QUERY = "SELECT email, password FROM admins WHERE email = ? AND password = ?";
+
+    public static boolean verificationOfAdminData (String email, String hashpass) {
+        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(SEARCH_FOR_EMAIL_AND_PASS_QUERY)) {
+            ps.setString(1, email);
+            ps.setString(2, hashpass);
+
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) return true;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return false;
+    }
 }
--- src/main/java/pl/coderslab/web/LoginServlet.java
+++ src/main/java/pl/coderslab/web/LoginServlet.java
@@ -1,18 +1,30 @@
+package pl.coderslab.web;
+
+import pl.coderslab.dao.AdminDao;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "LoginServlet", value = "/LoginServlet")
+@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        getServletContext().getRequestDispatcher("/login.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        // hashPassword  - metoda haszowania paroli
+
+        String hashPass = "hashPassword(request.getParameter(\"password\"))";
+
+        if (AdminDao.verificationOfAdminData(request.getParameter("email"), hashPass)) {
+            response.sendRedirect("");
+        } else {
+            response.sendRedirect("/login");
+        }
     }
 }
