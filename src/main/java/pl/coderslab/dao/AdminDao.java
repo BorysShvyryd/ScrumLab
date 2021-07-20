b66c6636455f55e5f181bccb328c52cc3ee7d75f
BorysShvyryd <bormanpgg@gmail.com>
2021-07-20 22:09:14 +0300

    Metodę pobierającą liczbę planów

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -37,7 +37,7 @@ public class AdminDao {
         return -1;
     }
 
-    private static final String NUMBER_RECIPE_QUERY = "SELECT COUNT(id) AS count_id FROM recipe WHERE admin_id = ?";
+    private static final String NUMBER_USER_RECIPE_QUERY = "SELECT COUNT(id) AS count_id FROM recipe WHERE admin_id = ?";
 
     /**
      * Metodę pobierającą liczbę przepisów dodanych przez aktualnie zalogowanego użytkownika
@@ -47,7 +47,27 @@ public class AdminDao {
      */
 
     public static int RecipeDao(int adminId) {
-        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(NUMBER_RECIPE_QUERY)) {
+        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(NUMBER_USER_RECIPE_QUERY)) {
+            ps.setInt(1, adminId);
+
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next())
+                return resultSet.getInt("count_id");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return 0;
+    }
+
+    private static final String NUMBER_USER_PLAN_QUERY = "SELECT COUNT(id) AS count_id FROM plan WHERE admin_id = ?";
+
+    /**
+     * Metodę pobierającą liczbę planów dodanych przez aktualnie zalogowanego użytkownika.
+     * @param adminId - id aktualnie zalogowanego użytkownika
+     * @return - liczbę planów
+     */
+    public static int PlanDao (int adminId) {
+        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(NUMBER_USER_PLAN_QUERY)) {
             ps.setInt(1, adminId);
 
             ResultSet resultSet = ps.executeQuery();
