f208ddd01a42e8e5a7d4f9b9cac4165aa80fd7b3
BorysShvyryd <bormanpgg@gmail.com>
2021-07-22 20:17:03 +0300

    zmiany lokalizacji metod

--- src/main/java/pl/coderslab/dao/AdminDao.java
+++ src/main/java/pl/coderslab/dao/AdminDao.java
@@ -36,46 +36,4 @@ public class AdminDao {
         }
         return -1;
     }
-
-    private static final String NUMBER_USER_RECIPE_QUERY = "SELECT COUNT(id) AS count_id FROM recipe WHERE admin_id = ?";
-
-    /**
-     * Metodę pobierającą liczbę przepisów dodanych przez aktualnie zalogowanego użytkownika
-     *
-     * @param adminId - id aktualnie zalogowanego użytkownika
-     * @return - liczbę przepisów
-     */
-
-    public static int RecipeDao(int adminId) {
-        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(NUMBER_USER_RECIPE_QUERY)) {
-            ps.setInt(1, adminId);
-
-            ResultSet resultSet = ps.executeQuery();
-            if (resultSet.next())
-                return resultSet.getInt("count_id");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return 0;
-    }
-
-    private static final String NUMBER_USER_PLAN_QUERY = "SELECT COUNT(id) AS count_id FROM plan WHERE admin_id = ?";
-
-    /**
-     * Metodę pobierającą liczbę planów dodanych przez aktualnie zalogowanego użytkownika.
-     * @param adminId - id aktualnie zalogowanego użytkownika
-     * @return - liczbę planów
-     */
-    public static int PlanDao (int adminId) {
-        try (PreparedStatement ps = DbUtil.getConnection().prepareStatement(NUMBER_USER_PLAN_QUERY)) {
-            ps.setInt(1, adminId);
-
-            ResultSet resultSet = ps.executeQuery();
-            if (resultSet.next())
-                return resultSet.getInt("count_id");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return 0;
-    }
 }
