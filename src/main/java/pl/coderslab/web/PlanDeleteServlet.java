d6ee8c7101f929a8de230ce4b00b316e4e24a5f6
BorysShvyryd <bormanpgg@gmail.com>
2021-07-28 23:21:54 +0300

    start deletePlan

--- src/main/java/pl/coderslab/web/PlanDeleteServlet.java
+++ src/main/java/pl/coderslab/web/PlanDeleteServlet.java
@@ -1,13 +1,18 @@
+package pl.coderslab.web;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
 import java.io.IOException;
 
-@WebServlet(name = "PlanDeleteServlet", value = "/PlanDeleteServlet")
+@WebServlet("/app/plan/delete")
 public class PlanDeleteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+//        confirm("Czy na pewno chcesz usunąć przepis z planu?");
 
     }
 
