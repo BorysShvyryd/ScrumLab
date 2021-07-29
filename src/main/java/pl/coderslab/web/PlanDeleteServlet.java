72621970a22302bafae928aac163748e8d81203a
BorysShvyryd <bormanpgg@gmail.com>
2021-07-29 17:30:49 +0300

    time save

--- src/main/java/pl/coderslab/web/PlanDeleteServlet.java
+++ src/main/java/pl/coderslab/web/PlanDeleteServlet.java
@@ -6,14 +6,26 @@ import javax.servlet.annotation.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/app/plan/delete")
 public class PlanDeleteServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-//        confirm("Czy na pewno chcesz usunąć przepis z planu?");
+        String someMessage = "Czy na pewno chcesz usunąć przepis z planu?";
 
+        response.setContentType("text/html");
+        response.getWriter().print(
+                "<%@ page contentType=\"text/html;charset=UTF-8\" language=\"java\" %>");
+//        response.getWriter().print(
+//                "<%@ taglib uri=\"http://java.sun.com/jsp/jstl/core\" prefix=\"c\"%>\n");
+        response.getWriter().print(
+                "<%@include file=\"headerLogined.jsp\"%>");
+        response.getWriter().print(
+                "<script type=\"text/javascript\">confirm(" + someMessage + ");</script>");
+        response.getWriter().print(
+                "<%@include file=\"footerLogined.jsp\"%>");
     }
 
     @Override
